40dffe71e705b00dc6c282df243d64739722c373
penger <kyrbynyt@gmail.com>
2025-08-09 07:48:51 +0000

    tests

--- tests/lexer_operators.c
+++ tests/lexer_operators.c
@@ -3,19 +3,20 @@
 #include <string.h>
 
 int main() {
-    char *src = "123 69\n 1";
+    char *src = "+ -/*";
     Lexer l = {.begin_of_src = src, .input_name = "CONST", .src = SV_FROM_CSTR(src)};
     Tokens out = {0};
     ASSERT(lexer_run(&l, &out), "The source code should be lexible without any errors");
 
 
     Token expected[] = {
-        (Token){.type = TT_NUMBER, .begin = src, .len = 3, .number = 123},
-        (Token){.type = TT_NUMBER, .begin = src + 4, .len = 2, .number = 69},
-        (Token){.type = TT_NUMBER, .begin = src + 8, .len = 1, .number = 1},
+        (Token){.type = TT_OPERATOR, .begin = src, .len = 1, .operator = OT_PLUS},
+        (Token){.type = TT_OPERATOR, .begin = src + 2, .len = 1, .operator = OT_MINUS},
+        (Token){.type = TT_OPERATOR, .begin = src + 3, .len = 1, .operator = OT_DIV},
+        (Token){.type = TT_OPERATOR, .begin = src + 4, .len = 1, .operator = OT_MULT},
     };
 
-    if (out.count < 3) return 1;
+    if (out.count < 4) return 1;
 
     for (size_t i = 0; i < out.count; i++) {
         if (memcmp(&expected[i], &out.items[i], sizeof(Token)) != 0) return 1;
