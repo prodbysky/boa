310661606dc825f963853f029b609c0740e5fd7b
penger <kyrbynyt@gmail.com>
2025-08-13 14:02:41 +0000

    alright

--- src/backend/codegen/nasm_x86_64_linux.c
+++ src/backend/codegen/nasm_x86_64_linux.c
@@ -1,10 +1,11 @@
 #include "nasm_x86_64_linux.h"
 #include "../../util.h"
+#include <stdio.h>
 
+static bool generate_function(FILE *sink, const IRFunction *func);
+static bool generate_statement(FILE *sink, const IRStatement *st);
 
-static bool generate_function(FILE* sink, const IRFunction* func);
-
-bool nasm_x86_64_linux_generate_file(FILE* sink, const IRModule* mod) {
+bool nasm_x86_64_linux_generate_file(FILE *sink, const IRModule *mod) {
     ASSERT(mod->functions.count == 1, "expect only one function for now");
     ASSERT(strcmp(mod->functions.items[0].name, "main") == 0, "expect only a main function");
     // prelude of some sorts
@@ -12,8 +13,9 @@ bool nasm_x86_64_linux_generate_file(FILE* sink, const IRModule* mod) {
     fprintf(sink, "global _start\n");
     fprintf(sink, "_start:\n");
     fprintf(sink, "  call main\n");
+    fprintf(sink, "  mov rsi, rax\n");
     fprintf(sink, "  mov rax, 60\n");
-    fprintf(sink, "  mov rdi, 0\n");
+    fprintf(sink, "  mov rdi, rsi\n");
     fprintf(sink, "  syscall\n");
 
     generate_function(sink, &mod->functions.items[0]);
@@ -21,16 +23,63 @@ bool nasm_x86_64_linux_generate_file(FILE* sink, const IRModule* mod) {
     return true;
 }
 
-static bool generate_function(FILE* sink, const IRFunction* func) {
+static bool generate_function(FILE *sink, const IRFunction *func) {
     fprintf(sink, "%s:\n", func->name);
-    if (func->max_temps != 0) {
-        fprintf(sink, "  enter %ld, 0\n", func->max_temps * 8);
-    }
+    fprintf(sink, "  enter %ld, 0\n", func->max_temps * 8);
+
+    for (size_t i = 0; i < func->body.count; i++) { generate_statement(sink, &func->body.items[i]); }
 
     fprintf(sink, "ret:\n");
     fprintf(sink, "  leave\n");
     fprintf(sink, "  ret\n");
 
     return true;
+}
+
+static bool generate_statement(FILE *sink, const IRStatement *st) {
+    switch (st->type) {
+    case IRST_RETURN: {
+        switch (st->ret.value.type) {
+        case IRVT_CONST: {
+            fprintf(sink, "  mov %s, %ld\n", "rax", st->ret.value.constant);
+            break;
+        }
+        case IRVT_TEMP: {
+            fprintf(sink, "  mov %s, [rbp - %ld]\n", "rax", (st->ret.value.temp + 1) * 8);
+            break;
+        }
+        }
+        fprintf(sink, "  jmp ret\n");
+        return true;
+    }
+    case IRST_ADD: {
+        ASSERT(st->binop.result.type == IRVT_TEMP, "we can't add to a constant");
+        switch (st->binop.l.type) {
+        case IRVT_CONST: {
+            fprintf(sink, "  mov %s, %ld\n", "rax", st->binop.l.constant);
+            break;
+        }
+        case IRVT_TEMP: {
+            fprintf(sink, "  mov %s, [rbp - %ld]\n", "rax", (st->binop.l.temp + 1) * 8);
+            break;
+        }
+        }
+
+        fprintf(sink, "  add rax, ");
+        switch (st->binop.r.type) {
+        case IRVT_CONST: {
+            fprintf(sink, "%ld\n", st->binop.r.constant);
+            break;
+        }
+        case IRVT_TEMP: {
+            fprintf(sink, "QWORD PTR [rbp - %ld]\n", (st->binop.r.temp + 1) * 8);
+            break;
+        }
+        }
+        fprintf(sink, "  mov [rbp - %ld], rax\n", (st->binop.result.temp + 1) * 8);
+        return true;
+    }
+    default: TODO();
+    }
 }
 
